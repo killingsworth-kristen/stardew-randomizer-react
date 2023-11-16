128d627369705a2ff204471f857a55b2a215dc61
kristen-killingsworth <killingsworth_kristen@yahoo.com>
2023-11-15 19:38:46 -0500

    header style first pass

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,21 @@
 import React from "react";
-
+import './style/Header.css'
 
 export default function Header () {
+
     return (
         <header className="header">
             <h1 className="title">Stardew Valley Perfection Randomizer</h1>
             <nav className="nav">
-                <button className="account-btn">Account</button>
-                <div className="account-drop">
-                    <div>
-                        <h6>Logout</h6>
-                    </div>
-                    <div>
-                        <h6>View My Saves</h6>
-                    </div>
-                    <div>
-                        <h6>About</h6>
-                    </div>
-                    <div>
-                        <h6>Help</h6>
+                <button className="menu-btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                        <h2>Menu</h2>
+                </button>
+                <div className="collapse" id="collapseExample">
+                    <div className="menu-dropdown">
+                        <a><h3>Logout</h3></a>
+                        <a><h3>Saves</h3></a>
+                        <a><h3>Help</h3></a>
+                        <a><h3>About</h3></a>
                     </div>
                 </div>
             </nav>
