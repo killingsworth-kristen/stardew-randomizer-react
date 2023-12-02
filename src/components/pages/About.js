302c8f446d523a3989de250a2550658fdf0d9da0
kristen-killingsworth <killingsworth_kristen@yahoo.com>
2023-12-01 21:57:28 -0500

    About page first pass

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,22 @@
 import React from "react";
 
+import '../style/About.css';
+
 export default function About () {
     return (
         <main>
             <h2 className="page-title">About</h2>
+            <div className="about-para-container">
+                <p className="about-para">
+                    All credit for this idea should go to ArgonMatrix who created the original Stardew Valley Perfection Randomizer in Google Sheets. You should definitely chack out his content on Youtube and check out his Randomizer VODs. 
+                </p>
 
+                <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/videoseries?si=vhIwUqmNx5pux9ht&amp;list=PL6-AYSf47-FsBiWANzLoaIMuTjs3k-XIM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+                <p className="about-para">
+                    I decided to take ArgonMatrix's idea and turn it into a full website, as I found the Google Sheet (while totally impressive) a bit cumbersome. Also, there were security pop-ups from Google, which I knew had the potential to scare away some users who may not be as tech/Google Sheets savvy. Once I thought about making it into a website, I had lots of ideas about how I could add to the usability of the randomizer and some fun extra features. I hope you enjoy using the randomizer!
+                </p>
+            </div>
         </main>
     )
 }
--- src/components/pages/Help.js
+++ src/components/pages/Help.js
@@ -8,14 +8,13 @@ export default function Help () {
             <h2 className="page-title">Help</h2>
             <section className="faq">
                 <div className="page-center">
-                    <h3><u><b>FAQ</b></u></h3>
                     <div>
-                        <h4><b>Q: How is my information stored?</b></h4>
-                        <p>Your data is stored only in your browser and is not sent to a server or anything else. You are in full control of your data.</p>
+                        <h4 className="help-question"><b>Q: How is my information stored?</b></h4>
+                        <p className="help-para">Your data is stored only in your browser and is not sent to a server or anything else. You are in full control of your data.</p>
                     </div>
                     <div>
-                        <h4><b>Q: How do I import my file from google sheets?</b></h4>
-                        <p>
+                        <h4 className="help-question"><b>Q: How do I import my file from google sheets?</b></h4>
+                        <p className="help-para">
                             <ol>
                                 <li>Go to your copy of Argon's Stardew Randomizer in your Google Drive</li>
                                 <li>Navigate to the "List of Goals" Tab at the bottom of the page</li>
